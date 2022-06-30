5ca85939679881ccab15115508dc18df06f814b4
Ahmed Attallah <101919660+TowebiaDev@users.noreply.github.com>
2022-06-30 17:26:35 +0200

    added verify

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
-const {JWT_KEY} = require("../config");
+const { JWT_KEY } = require("../config");
 
 module.exports = {
   def: (req, res) => {
@@ -36,7 +36,7 @@ module.exports = {
         res.status(500).send({ errors: `Saving in DB ERROR : ${err}` });
       });
 
-      res.status(201).send({ msg: "User added Successfuly" });
+      res.status(201).send({ msg: "User added Successfuly", user: user });
     } catch (err) {
       res.status(500).send({ errors: "Internal Server ERROR" });
     }
@@ -51,14 +51,32 @@ module.exports = {
 
       const token = jwt.sign({ phone_number }, JWT_KEY, {
         algorithm: "HS256",
-        expiresIn: 15,
+        expiresIn: 1500,
       });
       console.log("token:", token);
 
-      res.cookie("token", token, { maxAge: 15 * 1000 });
+      res.cookie("token", token, { maxAge: 1500 * 1000 });
       res.status(201).send({ msg: "Successfull", "auth-token": token });
     } catch (err) {
       res.status(500).send({ errors: `Internal Server ERROR : ${err}` });
     }
   }, // Return Token
+
+  verifying: async (req, res) => {
+    const {phone_number, token, status} = req.body;
+
+    if (!token) {
+      return res.status(401).send({ error: "Invalid Token" });
+    }
+
+    const user = await User.findOne({phone_number : phone_number});
+    const {first_name} = user;
+    try {
+      if (jwt.verify(token, JWT_KEY)) 
+      res.send(`Welcome ${first_name}!`);
+    } catch (err) {
+        return res.status(400).send({ errors: `Invalid Credintials ${err}` });
+    }
+
+  }, // Verify Auth
 };
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { def, addUser, auth } = require("../controllers");
+const { def, addUser, auth, verifying } = require("../controllers");
 const { addValidation } = require("../controllers/validator");
 
 router.get("/", def);
@@ -8,8 +8,13 @@ router.get("/", def);
 // @desc Create new User
 router.post("/adduser", addValidation, addUser);
 
-// @Route POST /adduser
-// @desc Create new User
+// @Route POST /auth
+// @desc create auth-token
 router.post("/auth", auth);
+
+
+// @Route POST /verify
+// @desc verify token
+router.post("/verify", verifying);
 
 module.exports = router;
